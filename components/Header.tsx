c9214606bca8f8944f0fd0316d7b9a2d523e966f
Joshua Carroll <carrolljosh1976@gmail.com>
2025-05-07 12:21:51 -0400

    added Header component, sign-in, sign-out buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { Button } from './ui/button'
+"use client";
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { Shield } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
+import { Button } from './ui/button';
 
 function Header() {
   return (
-    <div className=''>
-        <h1>
-            Header
-        </h1>
-        <Button>Hello</Button>
+    <div className='p-4 flex justify-between items-center'>
+        <Link href="/" className='flex items-center'>
+            <Shield className='w-6 h-6 text-blue-600 mr-2' />
+            <h1 className='text-xl font-semibold'>Expensio</h1>
+        </Link>
+
+        <div>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton>
+                    <Button>Login</Button>
+                </SignInButton>
+            </SignedOut>
+        </div>
         </div>
   )
 }
