ffeb6288597bf1469d7fa6d6ba6407309ec0e11a
Joshua Carroll <carrolljosh1976@gmail.com>
2025-05-06 21:21:13 -0400

    Initialized project, added button component

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+function Header() {
+  return (
+    <div className=''>
+        <h1>
+            Header
+        </h1>
+        <Button>Hello</Button>
+        </div>
+  )
+}
+
+export default Header
