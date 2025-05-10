9bfa5df9a5f9a3690a496c06a79ed0da5d7a7c1d
Joshua Carroll <carrolljosh1976@gmail.com>
2025-05-10 03:32:14 -0400

    added server action file getTemporaryTokenAccess

---
+++ actions/getTemporaryTokenAccess.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+// Initialize Schematic SDK
+import { SchematicClient } from "@schematichq/schematic-typescript-node";
+const apiKey = process.env.SCHEMATIC_API_KEY;
+const client = new SchematicClient({ apiKey });
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,30 @@ import { Shield } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
 
 function Header() {
+    const pathname = usePathname();
+    const isHomePage = pathname === '/';
   return (
-    <div className='p-4 flex justify-between items-center'>
+    <div className={`p-4 flex justify-between items-center` + (isHomePage ? ' bg-blue-50' : ' bg-white border-b border-blue-50')}>
         <Link href="/" className='flex items-center'>
             <Shield className='w-6 h-6 text-blue-600 mr-2' />
             <h1 className='text-xl font-semibold'>Expensio</h1>
         </Link>
 
-        <div>
+        <div className='flex items-center space-x-4'>
             <SignedIn>
+                <Link href="/receipts">
+                    <Button variant="outline">My Receipts</Button>
+                </Link>
+                <Link href="/manage-plan">
+                    <Button>Manage Plan</Button>
+                </Link>
                 <UserButton />
             </SignedIn>
             <SignedOut>
-                <SignInButton>
+                <SignInButton mode='modal'>
                     <Button>Login</Button>
                 </SignInButton>
             </SignedOut>
