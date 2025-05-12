d1a3f65794d82d4d08296afad49163f53c5dbcb4
Joshua Carroll <carrolljosh1976@gmail.com>
2025-05-11 20:53:21 -0400

    added header and mission statement to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col m-h-screen">
+      {/*Hero*/}
+        <section>
+          <div className="container px-4 md:px-6 mx-auto">
+            <div className="flex flex-col items-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
+                  Intelligent Receipt Scanning
+                </h1>
+                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                  Scan, analyze, and organize your receipts with AI-powered precision. Save time and gain insights from your expenses.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      {/*Features*/}
+
+      {/*Pricing*/}
+
+      {/*Info*/}
+
+      {/*Footer*/}
     </div>
   );
 }
